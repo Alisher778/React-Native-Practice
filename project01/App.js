4f277b3e69d543857e276cc3d515bb18651e8378
Alisher778 <alisher.musurmonov89@gmail.com>
2019-01-06 21:29:38 +0500

    adding new project

---
+++ project01/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return(
+      <Text>
+        Hello Man
+      </Text>
+    );
+  }
+}
