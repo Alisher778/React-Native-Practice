165c7132e32315b9805adee6e4852f7ce6c6f4f2
Alisher778 <alisher.musurmonov89@gmail.com>
2019-01-10 14:07:00 +0500

    added styling

--- project01/App.js
+++ project01/App.js
@@ -1,12 +1,51 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default class App extends Component {
+  state = {input: 'hey', k: ''}
+
+  inputHandler = val => {
+    this.setState({input: val});
+  }
+
+  value = (e) => {
+    alert(e.target.value)
+  }
   render() {
     return(
-      <Text>
-        Hello Man
-      </Text>
+      <View style={styles.font}>
+        <Text>{this.state.input}</Text>
+        <Text style={styles.fontText}>
+          Hello Man
+        </Text>
+        <TextInput value="" onChange={this.value} style={styles.input} />
+        <TextInput 
+			style={styles.input} 
+			value={this.state.input} 
+			onChangeText={this.inputHandler}
+			spellCheck={true}
+			autoCapitalize="sentences"
+		/>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  font: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: 200,
+    textAlign: 'center',
+    padding: 20
+  }, fontText: {
+    fontSize: 50,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderColor: '#000',
+    borderStyle: 'solid',
+    borderWidth: 1
+  }
+}) 
