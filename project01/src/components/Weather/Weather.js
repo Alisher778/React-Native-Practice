27d532dad9dd75c0034806081331e66e1ee0e771
Alisher778 <alisher.musurmonov89@gmail.com>
2019-01-14 22:58:42 +0500

    added text and view component

--- project01/src/components/Weather/Weather.js
+++ project01/src/components/Weather/Weather.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-
+export default Weather React.Component {
+    render() {
+        return(
+            <View>
+                <Text>Nice One</Text>
+            </View>
+        );
+    }
+}
