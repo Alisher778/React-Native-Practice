5d4b0046a71c43f5ccbea5ff86b206f133edae25
Alisher778 <alisher.musurmonov89@gmail.com>
2019-01-24 15:24:24 +0500

    added cameraroll function

--- project01/src/components/Users/Users.js
+++ project01/src/components/Users/Users.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'; 
+import { ActivityIndicator,Button, CameraRoll, FlatList, Image, StyleSheet, Text, View } from 'react-native'; 
 import axios from 'axios';
 
 export default class Users extends Component {
-    state = {users: [{key: 1, id: 2}], err: '', animating: true};
+    state = {users: [{key: 1, id: 2}], err: '', animating: true, imgs:[]};
 
     componentDidMount = () => { 
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -18,15 +18,33 @@ export default class Users extends Component {
             .catch(err => this.setState({err: err.message, animating: false})
             )
     }
+
+    cameraHandler = () => {
+        CameraRoll.getPhotos({
+                first: 10,
+                assetType: "All"
+            })
+            .then(photo => {
+                this.setState({imgs: [...this.state.imgs, photo]})
+            })
+    }
     render() {
         return(
             <View>
                 <Text>{this.state.err}</Text>
+                <Button 
+                    title="Learn More"
+                    color="#841584" 
+                    onPress={this.cameraHandler} 
+                />
+                <Image source={{uri: this.state.imgs[0]}} style={{width: 300, height: 200}} />
+
                 <ActivityIndicator size="large" color="red" animating={this.state.animating}/>
                 <FlatList
                     data={this.state.users}
                     renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
                 />
+                
             </View>
         );
     }
