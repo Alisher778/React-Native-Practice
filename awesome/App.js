ed5c74a149fdd2819c1331dc0805774917a0738c
Alisher778 <alisher.musurmonov89@gmail.com>
2019-02-10 21:52:56 +0500

    saving changes for redux

--- awesome/App.js
+++ awesome/App.js
@@ -9,7 +9,7 @@ import reducers from './src/store/store';
 
 const stores = createStore(reducers)
 
-export default class App extends Component{
+class App extends Component{
   constructor(props) {
     super(props);
     state = {users: [{name: 'Alisher', email: 'ali@gmail.com'}], msg: '', status: null}
@@ -36,7 +36,7 @@ export default class App extends Component{
     return (
       <Provider store={stores}>
         <View style={styles.container}>
-          <Text style={styles.welcome}>{this.props.users.users[0].name}</Text>
+          <Text style={styles.welcome}>{this.state.users[0].name}</Text>
           <AddUser />
           <UserModal />
           {/* <View style={{width: "100%", padding: 5,}}>{usersList}</View> */}
@@ -47,11 +47,11 @@ export default class App extends Component{
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.users
-  }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//     users: state.users
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -70,4 +70,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default connect(mapStateToProps)(App);
+export default App;
