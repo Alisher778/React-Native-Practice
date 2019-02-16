0e3f4c2b98099293be020cf19de8c7b86f4cd069
Alisher778 <alisher.musurmonov89@gmail.com>
2019-02-16 22:06:08 +0500

    app is working

--- crmAlicodeacademy/App.js
+++ crmAlicodeacademy/App.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default class App extends Component {
-  state = {name: ''};
+  state = {name: 'Hello'};
   render() {
     return (
       <View style={styles.container}>
         <Text>{this.state.name}</Text>
         <TextInput 
           onChangeText={(val) => this.setState({name: val})}
+          style={styles.border}
         />
       </View>
     );
@@ -20,5 +21,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  border: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'blue',
+    width: '90%'
   }
 });
